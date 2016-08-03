08c99f649855fd93379eec2a8cc8697e5ac0c60a
miandre <miandre@kth.se>
2016-08-03 22:32:47 +0200

    First working search engine!

--- src/com/company/IndexedWord.java
+++ src/com/company/IndexedWord.java
@@ -1,26 +1,102 @@
 package com.company;
 
-import java.util.HashMap;
+import java.io.Serializable;
+import java.util.*;
+import java.util.stream.Stream;
+
 
 /**
- * Created by Micke on 2016-08-02.
+ * Created by Mikael André on 2016-08-02.
+ *
+ * An object of this class is created for each word in the index of the search engine.
+ *
+ * The object contains a HashMap holding all documents in witch the current word occurs as the key, and the number of
+ * occurrences in that document as the value. It also holds the total number of occurrences oc the word in all indexed
+ * files. This may be used when normalizing the search result.
+ *
+ * The objec does not contain the actual word it represents, since each object is already mapped as the value to that
+ * specific ford in the index of the search engine.
  */
+
 public class IndexedWord {
 
-    String document;
-    HashMap<String,Integer> occurencies;
-    int totalOccurencies;
 
+    private HashMap<String,Integer> occurrences;
+    private int totalOccurrences;
+
+    /**
+     * Since an object is only created the first time a word occurs in any document, the constructor adds this words
+     * first occurence to the list of documents, with the value 1.
+     * @param document
+     */
     public IndexedWord(String document) {
-        this.document = document;
-        this.occurencies = new HashMap<>();
-        this.totalOccurencies = 1;
-        occurencies.put(document,1);
+        this.occurrences = new HashMap<>();
+        this.totalOccurrences = 1;
+        occurrences.put(document,1);
     }
 
-    public void addOccurency(String document) {
-
-        occurencies.put(document,occurencies.get(document)+1);
-        totalOccurencies++;
+    //******************GETTERS*****************/
+    public HashMap<String, Integer> getOccurrences() {
+        return occurrences;
     }
+
+    public int getTotalOccurrences() {
+        return totalOccurrences;
+    }
+    //*******************************************/
+
+
+    /**
+     * This method is called when an indexed word appears anew in a document where it have already been found.
+     * @param document
+     */
+    public void incrementOccurrence(String document) {
+
+        occurrences.put(document, occurrences.get(document)+1);
+        totalOccurrences++;
+    }
+
+    /**
+     * This method is called the first time an already indexed word is found in a new document.
+     * @param document
+     */
+    public void addOccurrence(String document) {
+
+        occurrences.put(document,1);
+        totalOccurrences++;
+    }
+
+
+    /**
+     *
+     * @return A LinkedHashMap() of the documents containing the search term, sorted according to the term frequency
+     * of the search term in each document.
+     *
+     * To sort the list, the Map of documents is converted to a Stream, and sorted into a LinkedHashMap, in descending
+     * order.
+     *
+     */
+    public Map<String, Integer> getDocumentList(){
+
+        Map<String, Integer> result = new LinkedHashMap<>();
+        Stream<Map.Entry<String, Integer>> stream = occurrences.entrySet().stream();
+
+        stream.sorted( idfComp() )
+                .forEachOrdered( e -> result.put(e.getKey(), e.getValue()) );
+
+        return result;
+
+    }
+
+    /**
+     * This comparator is used instead of the "Map.Entry.comparingByValue()" comparator, that sorts in ascending order.
+     * @param <K>
+     * @param <V>
+     * @return
+     */
+    private static <K, V extends Comparable<? super V>> Comparator<Map.Entry<K,V>> idfComp() {
+        return (Comparator<Map.Entry<K, V>> & Serializable)
+                (c1, c2) -> c2.getValue().compareTo(c1.getValue());
+    }
+
 }
--- src/com/company/SearchEngine.java
+++ src/com/company/SearchEngine.java
@@ -7,54 +7,126 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Micke on 2016-08-02.
+ *
+ * The SearchEngine object is the main part of the search engine.
+ * The constructor first creates the index that is used in the actual search.
+ * The index is in the form of a Hash map with the searchable words as keys,
+ * and a {@Link IndexedWord} object as the value.
+ *
+ * Then the {@Link indexFiles()} method is called to index the files in the specified folder.
+ * Finally, the search engine is implemented as an infinite loop where the user can search for single words.
+ *
  */
 public class SearchEngine {
 
-    HashMap<String, IndexedWord> index;
-    Path dir;
+    private HashMap<String, IndexedWord> index;
+    private Path dir;
 
     public SearchEngine(Path dir) throws IOException {
         this.index =new HashMap<>();
         this.dir = dir;
         indexFiles();
+        search();
     }
 
+    /**
+     * This method is used to index the files in the defined folders, in the form of an inverted index, where
+     * the searchable words are mapped to the documents where they can be found.
+     *
+     * The method extracts each line from a document as a String and then splits the string into words, ignoring
+     * all "non words" (i.e. whitespace characters, punctuations etc.)
+     *
+     * Each word is then indexed using the {@Link handleWord() method}.
+     *
+     * @throws IOException
+     */
     private void indexFiles() throws IOException {
 
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*.{txt}")) {
+
+            //go through all documents
             for (Path entry: stream) {
                 List<String> allLines = Files.readAllLines(entry);
-                String filename = entry.toString().split("\\W+")[1];
-                for (String s:allLines
-                        ) {
-                    String[] words = s.trim().split("\\W+");
-                    if(words.length>0) {
+                String documentName = entry.toString().split("\\W+")[1];
 
+                //Go through all lines in each document
+                for (String s:allLines) {
+                    String[] words = s.trim().split("\\W+");
+
+                    //Go through each word of a non empty line and index the word and document occurrence
+                    if(words.length>0) {
                         for (int i = 0; i<words.length; i++)
-                            addWord(words[i],filename);
+                            handleWord(words[i].toLowerCase(),documentName);
                     }
                 }
             }
+
         } catch (DirectoryIteratorException ex) {
-            // I/O error encounted during the iteration, the cause is an IOException
+            // I/O error encountered during the iteration, the cause is an IOException
             throw ex.getCause();
         }
 
     }
 
-    private void addWord(String word, String filename) {
+    /**
+     * This method is used to handle each word that are to be indexed.
+     *
+     * If the word has not occurred before in any document, the word is added as a key to the index,
+     * and an IndexedWord object is created as the corresponding value.
+     *
+     * If the word has already occurred in the same document, the frequency of the word in the current document
+     * is incremented.
+     *
+     * If the word has occured before, but not in the current document, the document is mapped to the word.
+     *
+     * @param word
+     * @param documentName
+     */
+    private void handleWord(String word, String documentName) {
 
+        //Add word to index
         if (!index.containsKey(word)) {
-            index.put(word, new IndexedWord(filename));
-            System.out.println("Indexing word / document: " + word+ " / " + index.get(word).document);
-            System.out.println("Occurencies in document: " + index.get(word).occurencies.get(filename));
-        }else {
-            index.get(word).addOccurency(filename);
-            System.out.println("Occurencies of "+word+ " : " + index.get(word).occurencies.get(filename));
+            index.put(word, new IndexedWord(documentName));
+
+
+            //increment the number off occurrences for the active document
+        }else if (index.get(word).getOccurrences().get(documentName)!=null) {
+            index.get(word).incrementOccurrence(documentName);
+
+
+            //Add a new document to the  word
+        }else{
+            index.get(word).addOccurrence(documentName);
         }
+    }
+
+
+    /**
+     * This method is the accual search methot that allows the user to search for a single word, and prints
+     * aa list of the documents where the word occurs, starting with the document with the most occurrences of the
+     * word.
+     *
+     * If the word is not present in any document, that fact is presented to the user. =)
+     */
+    private void search() {
+        while(true){
+            System.out.println("\nEnter a word: ");
+
+            Scanner input = new Scanner(System.in);
+
+            //Wait for user input
+            String phrase = input.next().trim();
+
+            if(index.get(phrase)!= null ) {
+                System.out.println("The word " + phrase + " can be found in the following documents: " + index.get(phrase).getDocumentList().keySet());
+            }else System.out.println("The word "+phrase+" can not be found in any document");
+
+        }
+
     }
 
 }
